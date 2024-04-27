8053f2e5b2378802ffcbf9548284579bd9162561
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-04-28 01:16:09 +0800

    first run

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     def __init__(self):
         # db_url 格式user:password@localhost/mydatabase
-        db_url = os.getenv("V2BoardDBUrl", "v2boardx.db")
+        db_url = os.getenv("V2BOARDDBURL", "v2boardx.db")
         self.db_connect_url = db_url
         cf_betterip_api = os.getenv("CFBetterIPApi", "https://yx.xmsl.io/vmess/all")
         self.cf_betterip_api = cf_betterip_api
